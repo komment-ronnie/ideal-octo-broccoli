929ff6c700a95307fb7dd4615b22a7cee1273752
komment-ronnie <153918503+komment-ronnie@users.noreply.github.com>
2024-07-23 11:46:24 +0100

    Create sample.ts

---
+++ sample.ts
@@ -0,0 +1,25 @@
+import React from 'react';
+const MathComponent: React.FC = () => {
+    function add(a: number, b: number): number {
+        return a + b;
+    }
+    function sin(a: number): number {
+        return Math.sin(a);
+    }
+    const results = [
+        add(5, 3),
+        sin(Math.PI / 2)
+    ];
+    return (
+        <div>
+            <h1>Math Results</h1>
+            <ul>
+                {results.map((result, index) => (
+                    <li key={index}>Result {index + 1}: {result}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default MathComponent;
